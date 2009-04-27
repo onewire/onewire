f0e37fc54d815da7eae25fcd65d08674c5ff506a
Aliaksandr Zlobich <zlobich.as@gmail.com>
2009-04-27 16:24:53 +0300

    Add scheduler interface and simple implementation.

---
+++ core/src/main/java/by/bsu/onewire/core/sheduler/Sheduler.java
@@ -0,0 +1,21 @@
+package by.bsu.onewire.core.sheduler;
+
+/**
+ * Common interface for scheduler object. Scheduler should collect task, and run
+ * them in correct order in 1-Wire context.
+ * 
+ * @author Aliaksandr Zlobich
+ * 
+ */
+public interface Sheduler {
+    
+    /**
+     * Add task in queue. This task will be executed later in 1-Wire context.
+     */
+    void addTask(Task task);
+    
+    /**
+     * Execute next task from queue.
+     */
+    void executeNextTask();
+}
