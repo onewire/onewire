7ad6b4942517c61c9e8047f9826673ddf44f46e3
Aliaksandr Zlobich <zlobich.as@gmail.com>
2009-04-30 18:52:02 +0300

    Create webui module, test application.

---
+++ webui/src/main/java/by/bsu/onewire/webui/server/GreetingServiceImpl.java
@@ -0,0 +1,18 @@
+package by.bsu.onewire.webui.server;
+
+import by.bsu.onewire.webui.client.GreetingService;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+/**
+ * The server side implementation of the RPC service.
+ */
+@SuppressWarnings("serial")
+public class GreetingServiceImpl extends RemoteServiceServlet implements GreetingService {
+
+    public String greetServer(String input) {
+        String serverInfo = getServletContext().getServerInfo();
+        String userAgent = getThreadLocalRequest().getHeader("User-Agent");
+        return "Hello, " + input + "!<br><br>I am running " + serverInfo + ".<br><br>It looks like you are using:<br>"
+                + userAgent;
+    }
+}
