a7b4ebcafdec25937d9a6d8510e81c56f78aed9a
Aliaksandr Zlobich <zlobich.as@gmail.com>
2009-05-26 00:29:05 +0300

    Javadocs comments

--- core/src/main/java/by/bsu/onewire/core/network/NetworkManager.java
+++ core/src/main/java/by/bsu/onewire/core/network/NetworkManager.java
@@ -7,18 +7,31 @@ import by.bsu.onewire.core.network.extensions.ConvertExtension;
 import by.bsu.onewire.core.network.extensions.SearchExtension;
 
 /**
+ * 1-Wire context, provide other modules access to network throught extentions.
  * 
  * @author Aliaksandr Zlobich
  * 
  */
 public interface NetworkManager {
 
+    /**
+     * Begin exclusive usage of 1-Wire network. 
+     */
     void startSession() throws OneWireIOException, OneWireException;
 
+    /**
+     * End exclusive usage of 1-Wire network. 
+     */
     void endSession() throws OneWireIOException, OneWireException;
 
+    /**
+     * Returns <code>SearchExtention</code>.
+     */
     SearchExtension getSearchExtension();
 
+    /**
+     * Returns <code>ConvertExtension</code>. 
+     */
     ConvertExtension getConvertExtension();
 
 }
