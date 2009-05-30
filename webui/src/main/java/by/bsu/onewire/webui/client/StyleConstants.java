785841439894a398ea7b2af5a4073dc622fdc068
Aliaksandr Zlobich <zlobich.as@gmail.com>
2009-05-30 23:44:27 +0300

    Add intarnationalization and styles support

---
+++ webui/src/main/java/by/bsu/onewire/webui/client/StyleConstants.java
@@ -0,0 +1,6 @@
+package by.bsu.onewire.webui.client;
+
+public class StyleConstants {
+    public static final String SIGNALING_LIST = "signaling-List";
+    public static final String SIGNALING_LIST_HEADER = "signaling-ListHeader";
+}
--- webui/src/main/java/by/bsu/onewire/webui/client/view/SignalingView.java
+++ webui/src/main/java/by/bsu/onewire/webui/client/view/SignalingView.java
@@ -3,8 +3,11 @@ package by.bsu.onewire.webui.client.view;
 import java.util.List;
 
 import by.bsu.onewire.core.modules.signaling.dto.SignalingElement;
+import by.bsu.onewire.webui.client.StyleConstants;
+import by.bsu.onewire.webui.client.i18n.SignalingConstants;
 import by.bsu.onewire.webui.client.model.SignalingModel;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Grid;
 import com.googlecode.gwtmvc.client.Controller;
@@ -13,18 +16,34 @@ import com.googlecode.gwtmvc.client.View;
 
 public class SignalingView extends View {
 
+    private static final int CONSTANT_ROWS_COUNT = 1;
+    private static final int HEADER_ROW_INDEX = 0;
+
+    private static final int COLUMNS_COUNT = 4;
+    private static final int ID_COLUMN_INDEX = 0;
+    private static final int TITLE_COLUMN_INDEX = 1;
+    private static final int STATUS_COLUMN_INDEX = 2;
+    private static final int STATE_COLUMN_INDEX = 3;
+
+    private SignalingConstants constants = (SignalingConstants) GWT.create(SignalingConstants.class);
+
     private Grid content;
 
     @SuppressWarnings("unchecked")
     public SignalingView(String key, Controller controller, ModelForView[] models) {
         super(key, controller, models);
         FlowPanel panel = new FlowPanel();
-        content = new Grid(1, 3);
+        content = new Grid(CONSTANT_ROWS_COUNT, COLUMNS_COUNT);
         content.setWidth("700px");
         content.setCellSpacing(0);
-        content.setCellPadding(0); 
-        content.addStyleName("signaling-List");
+        content.setCellPadding(0);
+        content.addStyleName(StyleConstants.SIGNALING_LIST);
         content.setVisible(false);
+        content.setHTML(HEADER_ROW_INDEX, ID_COLUMN_INDEX, constants.idColumn());
+        content.setHTML(HEADER_ROW_INDEX, TITLE_COLUMN_INDEX, constants.titleColumn());
+        content.setHTML(HEADER_ROW_INDEX, STATUS_COLUMN_INDEX, constants.statusColumn());
+        content.setHTML(HEADER_ROW_INDEX, STATE_COLUMN_INDEX, constants.stateColumn());
+        content.getRowFormatter().setStyleName(0, StyleConstants.SIGNALING_LIST_HEADER);
         panel.add(content);
         initWidget(panel);
     }
@@ -39,18 +58,17 @@ public class SignalingView extends View {
     public void onModelChange(ModelForView model) {
         if (model instanceof SignalingModel) {
             final SignalingModel signalingModel = (SignalingModel) model;
-            content.clear();
+
             final List<SignalingElement> elements = signalingModel.getValue();
-            content.resizeRows(elements.size()+1);
-            content.setHTML(0, 0, "Id");
-            content.setHTML(0, 1, "Enabled");
-            content.setHTML(0, 2, "Value");
-            content.getRowFormatter().setStyleName(0, "signaling-ListHeader");
-            int row = 1;
+            content.resizeRows(elements.size() + CONSTANT_ROWS_COUNT);
+            int row = CONSTANT_ROWS_COUNT;
             for (SignalingElement element : elements) {
-                content.setHTML(row, 0, Long.toString(element.getId()));
-                content.setHTML(row, 1, Boolean.toString(element.isEnabled()));
-                content.setHTML(row, 2, Boolean.toString(element.isAlarm()));
+                content.setHTML(row, ID_COLUMN_INDEX, Long.toString(element.getId()));
+                content.setHTML(row, TITLE_COLUMN_INDEX, element.getTitle());
+                final String status = element.isEnabled() ? constants.enabledStatus() : constants.disabledStatus();
+                content.setHTML(row, STATUS_COLUMN_INDEX, status);
+                final String state = element.isAlarm() ? constants.alarmState() : constants.normalState();
+                content.setHTML(row, STATE_COLUMN_INDEX, state);
                 ++row;
             }
             content.setVisible(true);
