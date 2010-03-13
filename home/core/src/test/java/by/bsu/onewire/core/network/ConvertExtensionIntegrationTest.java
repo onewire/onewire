a61f245640e4cdf7ab26acca6652ac6bc5c5d66d
Aliaksandr Zlobich <zlobich.as@gmail.com>
2010-03-13 23:47:29 +0200

    Addition integration tests for temperature reader

--- home/core/src/test/java/by/bsu/onewire/core/network/ConvertExtensionIntegrationTest.java
+++ home/core/src/test/java/by/bsu/onewire/core/network/ConvertExtensionIntegrationTest.java
@@ -2,11 +2,13 @@ package by.bsu.onewire.core.network;
 
 import javax.annotation.Resource;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import by.bsu.onewire.core.modules.monitoring.sensor.TemperatureSensorReader;
 import by.bsu.onewire.core.network.extensions.ConvertExtension;
 
 import com.dalsemi.onewire.OneWireException;
@@ -25,17 +27,44 @@ public class ConvertExtensionIntegrationTest {
 
     @Resource
     ConvertExtension convertExtension;
-    
+
     private static final double ALLOW_DIFF = 2;
+
+    private DSPortAdapter adapter;
+
+    private OneWireContainer10 device1;
+
+    private OneWireContainer10 device2;
+
+    private TemperatureSensorReader reader;
+
+    @Before
+    public void setUp() throws Throwable {
+        adapter = adapterProvider.getAdapter();
+        device1 = (OneWireContainer10) adapter.getDeviceContainer("11000800C63FE210");
+        device2 = (OneWireContainer10) adapter.getDeviceContainer("26000800C6773D10");
+        reader = new TemperatureSensorReader();
+        reader.setContainer(device1);
+    }
 
     @Test
     public void doConvertion() throws Throwable {
-        DSPortAdapter adapter = adapterProvider.getAdapter();
-        OneWireContainer10 device1 = (OneWireContainer10) adapter.getDeviceContainer("11000800C63FE210");
-        OneWireContainer10 device2 = (OneWireContainer10) adapter.getDeviceContainer("26000800C6773D10");
+
+        double beforeConversion = reader.readValue();
+        
         convertExtension.doTemperatureConversion();
-        double diff = readTemperature(device2) - readTemperature(device1);
-        assertThat("Difference between two temperatures should be less that 2 celsius", ((-ALLOW_DIFF < diff) && (diff < ALLOW_DIFF)), is(true));
+        double afterConvertion = readTemperature(device1);
+        
+        double readTemperature = readTemperature(device2);
+        double diff = readTemperature - afterConvertion;
+        assertThat("Difference between two temperatures should be less that 2 celsius", Math.abs(diff) < ALLOW_DIFF, is(true));
+        
+        diff = beforeConversion - afterConvertion;
+        assertThat("Difference between reader and extension should be less that 2 celsius", Math.abs(diff) < ALLOW_DIFF, is(true));
+        
+        reader.setForceConvertion(false);
+        diff = reader.readValue() - afterConvertion;
+        assertThat("There should be not difference", diff, is(0.0));
     }
 
     private double readTemperature(OneWireContainer10 device) throws OneWireIOException, OneWireException {
